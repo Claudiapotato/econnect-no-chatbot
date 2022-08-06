a4670bb25f04f9658c7811b129e85fdab5255a90
Claudiapotato <93199737+Claudiapotato@users.noreply.github.com>
2022-08-07 02:05:06 +0800

    heroku

---
+++ econnect-no-bot/index.php
@@ -0,0 +1,8 @@
+<?php 
+include_once("index.html");
+
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS, post, get');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+?>
+    
